bb9ba23cc05a1e17f595ab460eda7e73f9cd9bcd
Jan Rusak <01158887@pw.edu.pl>
2022-11-19 16:37:26 +0100

    Add ClinicalDiagnosis model

---
+++ Models/ClinicalDiagnosis.cs
@@ -0,0 +1,11 @@
+﻿namespace apka.Models
+{
+	public class ClinicalDiagnosis
+	{
+		public int Id { get; set; }
+
+		public string? DiagnosisName { get; set; }
+
+		public string? Remarks { get; set; }
+	}
+}
