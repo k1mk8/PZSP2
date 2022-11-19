9e2b2e8b77cd5004fbe4e35ed532a4ef687a4180
Jan Rusak <01158887@pw.edu.pl>
2022-11-19 16:56:11 +0100

    Add display names to ClinicalDiagnosis attributes

--- Models/ClinicalDiagnosis.cs
+++ Models/ClinicalDiagnosis.cs
@@ -1,11 +1,18 @@
-﻿namespace apka.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace apka.Models
 {
 	public class ClinicalDiagnosis
 	{
 		public int Id { get; set; }
 
+
+		[Display(Name = "Nazwa rozpoznania")]
 		public string? DiagnosisName { get; set; }
 
+
+		[Display(Name = "Uwagi")]
 		public string? Remarks { get; set; }
 	}
 }
