7a14beef4fe5cb5d2f67e86efd01c82c4e091809
Jan Rusak <01158887@pw.edu.pl>
2022-11-19 16:42:44 +0100

    Add ClinicalCenter model

---
+++ Models/ClinicalCenter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace apka.Models
+{
+	public class ClinicalCenter
+	{
+		public int Id { get; set; }
+
+
+		[Display(Name = "Nazwa osrodka")]
+		public string? CenterName { get; set; }
+
+
+		[Display(Name = "Adres")]
+		public string? CenterAddress { get; set;}
+
+
+		[Display(Name = "Opis")]
+		public string? CenterDescription { get; set; }
+	}
+}
