ae39c040e2920b0255ebfe7a0ef579e68e7f8a75
Jan Rusak <01158887@pw.edu.pl>
2022-12-04 22:10:27 +0100

    Add new attribute to Patient model and set decimal and date types

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace apka2.Models
 {
@@ -16,23 +17,31 @@ namespace apka2.Models
 		public DateTime BirthDate { get; set; }
 
 
+		[DataType(DataType.Date)]
 		[Display(Name = "Data hospitalizacji")]
 		public DateTime HospitalizationDate { get; set; }
 
 
+		[DataType(DataType.Date)]
 		[Display(Name = "Data rozpoczecia CKRT")]
 		public DateTime StartOfCKRTDate { get; set; }
 
 
 		[Display(Name = "Niewydolnosc watroby")]
-		public Boolean HasLiverFailure { get; set; }
+		public bool HasLiverFailure { get; set; }
+
+
+		[Display(Name = "Istotne klinicznie zaburzenia krzepniecia")]
+		public bool HasClinicallySignificantCoagulationDisorders { get; set; }
 
 
 		[Display(Name = "Masa ciala")]
+		[Column(TypeName = "decimal(6, 2)")]
 		public decimal Weight { get; set; }
 
 
 		[Display(Name = "Wzrost")]
+		[Column(TypeName = "decimal(6, 2)")]
 		public decimal Height { get; set; }
 
 
