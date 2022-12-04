fb2d14f20b38a421f111afbe3e647b386db0d03d
Jan Rusak <01158887@pw.edu.pl>
2022-12-04 14:12:19 +0100

    Add Procedure model

---
+++ Models/Procedure.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apka2.Models
+{
+	public class Procedure
+	{
+		public int Id { get; set; }
+
+
+		[Display(Name = "ECMO")]
+		public bool IsECMO { get; set; }
+
+
+		[Display(Name = "Zakonczony planowo")]
+		public bool EndedOnSchedule { get; set; }
+
+
+		[Display(Name = "Zwrocono krew")]
+		public bool BloodWasReturned { get; set; }
+
+
+		[Display(Name = "Powod nieplanowanego zakonczenia")]
+		public string? ReasonOfUnplannedTermination { get; set; }
+
+
+		[Display(Name = "Uwagi")]
+		public string? Remarks { get; set; }
+	}
+}
