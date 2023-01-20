3475df5b98749f9fe4e085f70e16f45e2f3d96cf
Jan Rusak <01158887@pw.edu.pl>
2023-01-20 12:38:06 +0100

    Added StartSessionDate

---
+++ Migrations/20230120113218_mainUpdate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apka2.Migrations
+{
+    /// <inheritdoc />
+    public partial class mainUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "initial",
+                table: "ProcedureSession",
+                newName: "Initial");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartSessionDate",
+                table: "ProcedureSession",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartSessionDate",
+                table: "ProcedureSession");
+
+            migrationBuilder.RenameColumn(
+                name: "Initial",
+                table: "ProcedureSession",
+                newName: "initial");
+        }
+    }
+}
