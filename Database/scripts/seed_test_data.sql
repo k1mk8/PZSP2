8b445c449d39a113c01e9f17385d576fec492daa
mwasilew <01158977@pw.edu.pl>
2023-01-22 22:53:09 +0100

    Updated some database tables names

--- Database/scripts/seed_test_data.sql
+++ Database/scripts/seed_test_data.sql
@@ -10,6 +10,6 @@ INSERT INTO database."DOCTORS" ("FIRST_NAME", "LAST_NAME", "IS_DATA_ADMINISTRATO
 INSERT INTO database."CLINICAL_DATA" ("PATIENT_ID", "CLINICAL_DATA_TYPE_CODE", "CLINICAL_DATA_TYPE_DISCRIMINATOR", "DATA_COLLECTION_DATE", "DOCTOR_ID")
 	VALUES (1, 25, 'C', '2023-01-07', 1);
 	
-INSERT INTO database."CLINICAL_DATA_PARAMETERS_VALUES" VALUES (1, 25, 'C', '2023-01-07', 161, 0, NULL, NULL, 'Nadciśnienie tętnicze...');
-INSERT INTO database."CLINICAL_DATA_PARAMETERS_VALUES" VALUES (1, 25, 'C', '2023-01-07', 173, NULL, 4, NULL, 'Grubość cewnika...');
-INSERT INTO database."CLINICAL_DATA_PARAMETERS_VALUES" VALUES (1, 25, 'C', '2023-01-07', 175, NULL, NULL, 'Żyła szyjna wewnętrzna prawa', 'Miejsce założenia dostępu naczyniowego...');
+INSERT INTO database."CLINICAL_DATA_VALUES" VALUES (1, 25, 'C', '2023-01-07', 161, 0, NULL, NULL, 'Nadciśnienie tętnicze...');
+INSERT INTO database."CLINICAL_DATA_VALUES" VALUES (1, 25, 'C', '2023-01-07', 173, NULL, 4, NULL, 'Grubość cewnika...');
+INSERT INTO database."CLINICAL_DATA_VALUES" VALUES (1, 25, 'C', '2023-01-07', 175, NULL, NULL, 'Żyła szyjna wewnętrzna prawa', 'Miejsce założenia dostępu naczyniowego...');
